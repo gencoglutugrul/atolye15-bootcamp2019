dc06f237ace4b75d39d022fec91d08cd2ff8b96e
Tuğrul Gençoğlu <gencoglutugrul@gmail.com>
2022-05-31 02:36:46 +0300

    Used Listr package instead of ora

--- bin/squareci.ts
+++ bin/squareci.ts
@@ -7,75 +7,85 @@ import {
   stopContainer
 } from '../lib/docker'
 
+import Listr from 'listr'
 import args from '../lib/args'
 import { getConfig } from '../lib/config'
-import ora from 'ora'
-import { runWithSpinner } from '../lib/worker'
 
-let container
 const main = async (): Promise<void> => {
   const argv = await args()
   const config = await getConfig(argv.config)
-  container = await runContainer(
-    config.image,
-    argv.path,
-    ora('Running Up Container\nMounting Working Path as volume')
-  )
 
-  const beforeTasks = config.before.map(
-    t => execOnContainer(container, t.run, ora({ text: t.name, indent: 4 }))
-  )
-  try {
-    await runWithSpinner(
-      beforeTasks,
-      ora('Running before stage'),
-      { stopOnFailure: config.exitOnFailure }
-    )
-  } catch (_) {
-    throw Error('Failed on running before stage!')
-  }
-
-  const taskList = config.steps.map(
-    t => execOnContainer(
-      container,
-      t.run,
-      ora({ text: t.name, indent: 4 })
-    )
-  )
-
-  await runWithSpinner(
-    taskList,
-    ora('Running steps'),
+  const tasks = new Listr([
     {
-      parallel: config.runInParallel,
-      stopOnFailure: config.exitOnFailure
+      title: 'Running Up Container',
+      task: (ctx) =>
+        runContainer(
+          config.image,
+          argv.path
+        ).then(container => {
+          ctx.container = container
+        })
+    },
+    {
+      title: 'Running Before Stage',
+      task: (ctx) => {
+        return new Listr(config.before.map(
+          task => ({
+            title: task.name,
+            task: () => execOnContainer(ctx.container, task.run)
+          })
+        ), { exitOnError: true })
+      }
+    },
+    {
+      title: 'Running Steps',
+      task: (ctx) => new Listr(config.steps.map(
+        task => ({
+          title: task.name,
+          task: () => execOnContainer(ctx.container, task.run)
+        })
+      ), {
+        exitOnError: config.exitOnFailure,
+        concurrent: config.runInParallel
+      })
+    },
+    {
+      title: 'Running After Stage',
+      task: (ctx) => new Listr(config.after.map(
+        task => ({
+          title: task.name,
+          task: () => execOnContainer(ctx.container, task.run)
+        })
+      ))
     }
-  )
+  ])
+  let container = null
+  tasks.run({
+    container
+  })
+    .then((ctx) => {
+      container = ctx.container
+    })
+    .catch(err => {
+      container = err.context.container
+      console.error(err.message)
+    })
+    .finally(() => {
+      if (container !== null) {
+        const cleanUp = new Listr([
+          {
+            title: 'Stopping Container',
+            task: () => stopContainer(container)
+          },
+          {
+            title: 'Removing Container',
+            task: () => removeContainer(container)
+          }
+        ])
 
-  const afterTasks = config.after.map(
-    t => execOnContainer(
-      container,
-      t.run,
-      ora({ text: t.name, indent: 4 })
-    )
-  )
-
-  await runWithSpinner(
-    afterTasks,
-    ora('Running after steps'),
-    { stopOnFailure: config.exitOnFailure }
-  )
-
-  await stopContainer(container, ora('Stopping container'))
-  await removeContainer(container, ora('Removing container'))
+        cleanUp.run().catch(e => console.error(e.message))
+      }
+    })
 }
 
 main()
-  .catch(console.error)
-  .finally(() => {
-    stopContainer(container, ora('Trying to stop container')).then(res => {
-      if (res) {
-        removeContainer(container, ora('Trying to remove container'))
-      }
-    })
-  })
